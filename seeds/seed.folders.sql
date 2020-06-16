90ce8b776a4b4f02fff0db52443c5822ca2340b4
Krissy <krissysaenz@gmail.com>
2020-06-15 21:49:05 -0700

    first commit

---
+++ seeds/seed.folders.sql
@@ -0,0 +1,12 @@
+INSERT INTO folders (folder_name)
+VALUES
+    ('Animals'),
+    ('Vehicles'),
+    ('Fruits'),
+    ('Veggies'),
+    ('Desserts'),
+    ('Tasks'),
+    ('Stores'),
+    ('Clothes'),
+    ('Toys');
+    
---
+++ src/folders/folders-router.js
@@ -0,0 +1,96 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const FoldersService = require('./folders-service')
+
+const folderRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeFolder = folder => ({
+    id: folder.id,
+    folder_name: xss(folder.folder_name)
+})
+
+folderRouter
+    .route('/')
+    .get((req, res, next) => {
+        const knexInstance = req.app.get('db')
+        FoldersService.getAllFolders(knexInstance)
+            .then(folders => {
+                res.json(folders.map(serializeFolder))
+            })
+            .catch(next)
+    })
+    .post(jsonParser, (req, res, next) => {
+        const folder_name = req.body.folder_name
+        if(folder_name === null){
+            return res.status(400).json({
+                error: { message: `Missing folder name in request body` }
+            })
+        }
+
+        FoldersService.insertFolder(
+            req.app.get('db'),
+            { folder_name }
+        )
+            .then(folder => {
+                res
+                    .status(201)
+                    .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+                    .json(serializeFolder(folder))
+            })
+            .catch(next)
+    })
+
+folderRouter
+    .route('/:folder_id')
+    .all((req, res, next) => {
+        FoldersService.getById(
+            req.app.get('db'),
+            req.params.folder_id
+        )
+        .then(folder => {
+            if(!folder){
+                return res.status(404).json({
+                    error: { message: `Folder doesn't exist` }
+                })
+            }
+            res.folder = folder
+            next()
+        })
+        .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json(serializeFolder(res.folder))
+    })
+    .delete((req, res, next) => {
+        FoldersService.deleteFolder(
+            req.app.get('db'),
+            req.params.folder_id
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+    .patch(jsonParser, (req, res, next) => {
+        const folder_name = req.body.folderName
+
+        if(folder_name === null){
+            return res.status(400).json({
+                error: { message: `Please update folder name`}
+            })
+        }
+
+        FoldersService.updateFolder(
+            req.app.get('db'),
+            req.params.folder_id,
+            { folder_name }
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+
+    module.exports = folderRouter
